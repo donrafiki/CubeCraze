59029aa54d5790e5a9df2b1005d8122b11f04382
Leonardo Almonti <leonardo.almonti@gmail.com>
2018-08-23 15:07:39 +0900

    Change blue objects addforce and added time delay change of scene at the end of tutorial

---
+++ Assets/Scripts/ChangeSceneTimeLevel1.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+ 
+public class ChangeSceneTimeLevel1 : MonoBehaviour
+ {
+     public float delay = 5;
+     void Start()
+     {
+         StartCoroutine(LoadLevelAfterDelay(delay));
+     }
+ 
+     IEnumerator LoadLevelAfterDelay(float delay)
+     {
+         yield return new WaitForSeconds(delay);
+         SceneManager.LoadScene(3);
+     }
+ }
